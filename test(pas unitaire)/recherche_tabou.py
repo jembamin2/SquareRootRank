477ae783aedbf004da1693b7eadbc24f8a2a92a2
ayselatx <ayseldamoiseau@gmail.com>
2024-11-28 19:09:46 +0100

    fix problem de i,j et de sortir bonne best_mask

--- test(pas unitaire)/recherche_tabou.py
+++ test(pas unitaire)/recherche_tabou.py
@@ -17,6 +17,10 @@ def rankof(array, tol=10**-10):
             count+=1
     return count
 
+def hash_matrix(matrix):
+    # Convertir la matrice en une chaîne et calculer un hash
+    matrix_str = str(matrix)
+    return hashlib.md5(matrix_str.encode()).hexdigest()
 
 def read_matrix(input):
     with open(input,'r') as fin:
@@ -29,68 +33,63 @@ def read_matrix(input):
             tmp=fin.readline().strip().split()
     return np.array(matrix)
 
-def voisinage(matrices, sqrt_matrix, mask, r, a):
+def voisinage(masks, matrix, mask, r):
     best_mask = mask
     best_r = r
-    for i in range(a[0]):
-        for j in range(a[1]):
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
             mask[i][j] = -1 * mask[i][j]
-            matrix_hash = hash_matrix(mask)
+            mask_hash = hash_matrix(mask)
 
             # Vérifier si la matrice existe déjà dans le dictionnaire via son hash
-            if matrix_hash not in matrices:
-                print("new mask")
-                matrices[matrix_hash] = mask  # Stocker la matrice seulement si elle est nouvelle
+            if mask_hash not in masks:
+                masks[mask_hash] = mask  # Stocker la matrice seulement si elle est nouvelle
             
-                U,S,Vh = np.linalg.svd(sqrt_matrix*mask, full_matrices= False)
+                U,S,Vh = np.linalg.svd(matrix*mask, full_matrices= False)
                 rank = rankof(S)
                 S = np.diag(S)
                 
-                #print(S)
-                #print(f"mask \n {mask}")
-                #print(f"rank {rank}")
-                #print(((U@S)@Vh)**2)
                 
-                if np.allclose((((U@S)@Vh))**2, sqrt_matrix**2, atol=1e-10):
-                    print("yes")
+                if np.allclose((((U@S)@Vh))**2, matrix**2, atol=1e-10):
                     if rank <= r:
                         print(f" new rank {rank}")
-                        best_mask = mask
+                        print(f" new mask {mask}")
+                        best_mask = mask.copy()
                         best_r = rank
-                        
-                
+                #### if never updated => force an update
                 # if ( i==a[0] and j == a[1] and best_mask.size() == 0):
                 #     best_mask = mask
                 #     best_r = rank
                     
                 mask[i][j] = -1 * mask[i][j]
+
+
+    return masks, best_mask, best_r
+ 
+        
+def recherche_tabou(matrix, r):
+    sqrt_matrix=np.zeros(matrix.shape)
+    sqrt_matrix=np.sqrt(matrix)
     
+    masks = {}              # dictionnaire hashé de tout les masks déjà testé
     
-        return matrices, best_mask, best_r
-    
-    
-def recherche_tabou(sqrt_matrix, r):
-    a = sqrt_matrix.shape
-    matrices = {}
-    #best_S=np.zeros(a)
-    best_mask=np.ones(a)
-    best_r = a[0]
+    mask=np.ones(matrix.shape)
+    rank = matrix.shape[0]
     
     i = 0
-    matrices, best_mask, best_r = voisinage(matrices, sqrt_matrix, best_mask, best_r, a)
+    matrices, best_mask, best_rank = voisinage(masks, sqrt_matrix, mask, rank)
             
-    if best_r >= r:
-        print(i)
-        matrices, best_mask, best_r = voisinage(matrices, sqrt_matrix, best_mask, best_r, a)
-        i+=1
-     
-    return best_mask, best_r
+    #while i < matrix.shape[0] * matrix.shape[1]:
+    while i < 2:
+        if best_rank >= r:
+            print(i)
+            matrices, best_mask, best_rank = voisinage(masks, sqrt_matrix, best_mask, best_rank)
+            i+=1
+    print(best_mask)
+    return best_mask, best_rank
 
 
-def hash_matrix(matrix):
-    # Convertir la matrice en une chaîne et calculer un hash
-    matrix_str = str(matrix)
-    return hashlib.md5(matrix_str.encode()).hexdigest()
+
 
      
 #%%
@@ -98,10 +97,9 @@ def hash_matrix(matrix):
 matrix = np.array([[9, 4],[16,9]])
 
 matrix = read_matrix("exempleslide_matrice.txt")
-sqrt_matrix=np.zeros(matrix.shape)
-sqrt_matrix=np.sqrt(matrix)
 
 
-best_mask, best_r = recherche_tabou(sqrt_matrix, np.shape(sqrt_matrix)[0])
+
+best_mask, best_r = recherche_tabou(matrix, np.shape(matrix)[0])
 print(f"best mask \n {best_mask}")
 print(f"best rank {best_r}")
