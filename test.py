88111f308c3932ea3da462eeef2dcec5f8abbcf5
Hao Ye <yehao10@gmail.com>
2024-12-12 14:50:01 +0100

    Replace matrix initialization with random matrix generation and save to file

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-x = np.array([[1, 2, 3,4], [4, 5, 6,4], [7, 8, 9,10]])
+randMat = np.random.rand(50, 50)
 
-y = np.linalg.norm(x, ord='fro')
+np.savetxt('file.txt', randMat)
 
-print(y)  # Output: 16.881943016134134
-
-print(np.random.choice([1,-1], size=(5,5)) )
+print("randMat has been saved to file.txt")
